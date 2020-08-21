4d1ec5bc0013905d9e423624ffbc71c438f550a6
shivam <63141401+shivam12k@users.noreply.github.com>
2020-08-21 16:14:54 +0530

    adding heading

--- result.js
+++ result.js
@@ -1,7 +1,12 @@
 let name = sessionStorage.getItem("name");
 let num = sessionStorage.getItem("num");
+let result = document.querySelector('.result');
 console.log(name);
 console.log(num)
 let reset = () =>{
     location.href="index.html";
 }
+setTimeout(()=>{
+   result.innerHTML=`hey ${name}  
+   your number is ${num}`;
+},1000)
